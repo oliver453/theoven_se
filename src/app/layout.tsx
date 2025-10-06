c9fd0bf982f2e2928cc3d8c38d879887b9e474c5
Oliver Thyberg <kontakt.oliverthyberg@gmail.com>
2025-10-06 14:16:01 +0200

    Lägger till SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Viewport } from 'next';
 import './globals.css';
 import { rusticPrinted, robotoSlab } from './fonts';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
